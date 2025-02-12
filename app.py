95cae5002c289b65ae13e98b1ea44128a88408f9
Alvyn Abranches <alvynabranches@gmail.com>
2025-02-12 15:57:23 +0530

    1202

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class Data(BaseModel):
+    price: float
+    quantity: int
+
+app = FastAPI()
+
+@app.post("/calculate_total_price")
+async def calculate_total_price(data: Data):
+    return {"total_price": data.price * data.quantity}
